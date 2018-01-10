7c6449bc606608f5354101b4f228a884c515c345
Jason Wang <jason@wangjason.com>
2018-01-10 10:51:43 -0500

    some comments

--- server/index.js
+++ server/index.js
@@ -5,16 +5,16 @@ var chalk = require('chalk');
 var volleyball = require('volleyball');
 var bodyParser = require('body-parser');
 
-app.use(volleyball);
-app.use(bodyParser.json());
+app.use(volleyball); // logging middleware
+app.use(bodyParser.json()); // body parsing middleware
 
-app.use('/api', require('./api'));
+app.use('/api', require('./api')); // api routes
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-});
+}); // anything that is not the matching URI to anything we declared in api or files in public, we will default to sending the index.html
 
 app.listen(8000, function () {
   console.log(chalk.magenta('Server is running on port 8000'));
